9edf24c2c34c5435ecf8b0fa294db6d4cb2b17cf
Arcenio Courtar <71286201+ArcenioCourtar@users.noreply.github.com>
2023-06-01 17:14:58 +0200

    initial commit

---
+++ philo/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acourtar <acourtar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/01 16:59:14 by acourtar          #+#    #+#             */
+/*   Updated: 2023/06/01 17:13:42 by acourtar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <pthread.h>	// pt_create, detach, join. mut_init, destr, lock, unl.
+#include <string.h>		// memset(),
+#include <stdio.h>		// printf()
+#include <stdlib.h>		// malloc(), free()
+#include <unistd.h>		// write(), usleep(),
+#include <sys/time.h>	// gettimeofday()
+
+int	main(int argc, char **argv)
+{
+	
+}
